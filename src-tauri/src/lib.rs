fb7a17c539e4400c5e4d400913ecb25e97ba1d59
João Batista <joao-oliveirabatista@hotmail.com>
2025-01-29 11:02:47 +0000

    cleanup unsued imports

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -27,11 +27,7 @@ pub fn run() {
 
 			Ok(())
 		})
-		.plugin(tauri_plugin_shell::init())
-		.plugin(tauri_plugin_notification::init())
-		.plugin(tauri_plugin_os::init())
-		.plugin(tauri_plugin_fs::init())
-		.plugin(tauri_plugin_store::Builder::new().build())
+
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
