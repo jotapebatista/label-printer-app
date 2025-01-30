5be069c7aa1e8e383f0d2bc3b2c77da712a01f5c
João Batista <joao-oliveirabatista@hotmail.com>
2025-01-30 12:12:34 +0000

    added auto app update feature

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,33 +1,38 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-
 use tauri::{
-	menu::{Menu, MenuItem},
-	tray::TrayIconBuilder
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
 };
+
+use tauri_plugin_updater;
+use tauri_plugin_dialog;
+use tauri_plugin_process;
 
 pub fn run() {
     tauri::Builder::default()
-		.setup(|app| {
-			let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-			let menu = Menu::with_items(app, &[&quit_i])?;
+        .setup(|app| {
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
 
-			let _tray = TrayIconBuilder::new()
-				.menu(&menu)
-				.menu_on_left_click(true)
-				.icon(app.default_window_icon().unwrap().clone())
-				.on_menu_event(|app, event| match event.id.as_ref() {
-					"quit" => {
-						app.exit(0);
-					}
-					other => {
-						println!("menu item {} not handled", other);
-					}
-				})
-				.build(app)?;
-
-			Ok(())
-		})
-
+            let _tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .menu_on_left_click(true)
+                .icon(app.default_window_icon().unwrap().clone())
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    other => {
+                        println!("menu item {} not handled", other);
+                    }
+                })
+                .build(app)?;
+				#[cfg(desktop)]
+				app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
+            Ok(())
+        })
+		.plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_process::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
